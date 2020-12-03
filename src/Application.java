fd4ee0d27dd1e73364ece8740e9d729c78ad9266
lgrill <linette.grill@mercadolibre.com>
2020-12-03 15:12:58 -0300

    Barrio no soportado

--- src/Application.java
+++ src/Application.java
@@ -29,21 +29,15 @@ public class Application {
         while (!barrio.equals("salir")) {
             System.out.println("Bienvenido, seleccione un barrio:");
             System.out.println("1. Carrasco");
-            System.out.println("2. Punta Gorda");
-            System.out.println("3. Malvín");
-            System.out.println("4. Buceo");
-            System.out.println("5. Pocitos");
-            System.out.println("6. Punta Carretas");
-            System.out.println("7. Parque Rodó");
-            System.out.println("8. Palermo");
-            System.out.println("9. Parque Batlle");
-            System.out.println("10. Cordón");
-            System.out.println("11. Centro");
-            System.out.println("12. Ciudad Vieja");
-            System.out.println("13. Aguada");
-            System.out.println("14. Prado");
-            System.out.println("15. Peñarol");
-            System.out.println("16. La blanqueada");
+            System.out.println("2. Malvín");
+            System.out.println("3. Buceo");
+            System.out.println("4. Pocitos");
+            System.out.println("5. Palermo");
+            System.out.println("6. Cordón");
+            System.out.println("7. Centro");
+            System.out.println("8. Aguada");
+            System.out.println("9. Prado");
+            System.out.println("10. Peñarol");
             System.out.println("salir");
             System.out.println();
             System.out.print("Ingrese el numero de barrio: ");
@@ -52,84 +46,62 @@ public class Application {
             barrio = reader.readLine();
 
             if (!barrio.equals("salir")) {
-                System.out.print("Ingrese la cantidad de dormitorios: ");
-                dormitorios = reader.readLine();
-
-                System.out.print("Ingrese la cantidad de baños: ");
-                banios = reader.readLine();
-
                 switch (barrio) {
                     case "1":
                         idBarrio = "TUxVQ0NBUmRhYWU0";
                         nombreBarrio = "Carrasco";
                         break;
                     case "2":
-                        idBarrio = "TUxVQ1BVTjYxODI";
-                        nombreBarrio = "Punta Gorda";
-                        break;
-                    case "3":
                         idBarrio = "TUxVQ01BTDE0YmY1";
                         nombreBarrio = "Malvin";
                         break;
-                    case "4":
+                    case "3":
                         idBarrio = "TUxVQ0JVQzNlMDdl";
                         nombreBarrio = "Buceo";
                         break;
-                    case "5":
+                    case "4":
                         idBarrio = "TUxVQ1BPQzM5ZGRi";
                         nombreBarrio = "Pocitos";
                         break;
-                    case "6":
-                        idBarrio = "TUxVQ1BVTjJmMjkx";
-                        nombreBarrio = "Punta Carretas";
-                        break;
-                    case "7":
-                        idBarrio = "TUxVQ1BBUmU3Y2Nj";
-                        nombreBarrio = "Parque Rodo";
-                        break;
-                    case "8":
+                    case "5":
                         idBarrio = "TUxVQ1BBTDU0NzY";
                         nombreBarrio = "Palermo";
                         break;
-                    case "9":
-                        idBarrio = "TUxVQ1BBUjVkNGE4";
-                        nombreBarrio = "Parque Batlle";
-                        break;
-                    case "10":
+                    case "6":
                         idBarrio = "TUxVQ0NPUjZmZjNm";
                         nombreBarrio = "Cordon";
                         break;
-                    case "11":
+                    case "7":
                         idBarrio = "TUxVQ0NFTjVjMTM";
                         nombreBarrio = "Centro";
                         break;
-                    case "12":
-                        idBarrio = "TUxVQ0NJVTk5MTU";
-                        nombreBarrio = "Ciudad Vieja";
-                        break;
-                    case "13":
+                    case "8":
                         idBarrio = "TUxVQ0FHVWUwMzc3";
                         nombreBarrio = "Aguada";
                         break;
-                    case "14":
+                    case "9":
                         idBarrio = "TUxVQ1BSQTYwOTJl";
                         nombreBarrio = "Prado";
                         break;
-                    case "15":
+                    case "10":
                         idBarrio = "TUxVQ1BF0TUxNDI";
                         nombreBarrio = "Peñarol";
                         break;
-                    case "16":
-                        idBarrio = "TUxVQ0xBWjk5YTE5";
-                        nombreBarrio = "La Blanqueada";
-                        break;
                     default:
                         System.out.println("Barrio no soportado");
+                        System.out.println();
                 }
 
-                System.out.println("Barrio seleccionado: "+ nombreBarrio);
-
                 if (idBarrio != null) {
+                    System.out.println("Barrio seleccionado: "+ nombreBarrio);
+                    System.out.println();
+
+                    System.out.print("Ingrese la cantidad de dormitorios: ");
+                    dormitorios = reader.readLine();
+
+                    System.out.print("Ingrese la cantidad de baños: ");
+                    banios = reader.readLine();
+
                     MercadoLibreController mercadoLibreController = new MercadoLibreController();
                     List<MercadoLibreProperty> inmueblesMeli = mercadoLibreController.LoadAlojamientos(idBarrio);
 
@@ -147,8 +119,6 @@ public class Application {
                     System.out.println();
                     System.out.println("-------------------------------------------------------------------------");
                     System.out.println();
-                } else {
-                    barrio = reader.readLine();
                 }
             }
         }
